403a1f346fbf29de36474c685df1bc7091b4f9f0
GustavoValdesZ <gustavovaldes.z@gmail.com>
2026-02-26 12:50:09 -0300

    faltaban importaciones

--- cuestionario/logica/manejo_archivos.py
+++ cuestionario/logica/manejo_archivos.py
@@ -1,8 +1,13 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
+from django.http import HttpResponse
+from cuestionario.models import Biblioteca
+
+
 @login_required
 def ver_archivo_biblioteca(request, biblioteca_id):
     if not request.user.is_superuser:
         return redirect('index')
-    from cuestionario.models import Biblioteca
     try:
         doc = Biblioteca.objects.get(id_biblioteca=biblioteca_id)
     except Biblioteca.DoesNotExist:
